43e72c20100831df9e824ffac0d397a7b76697d5
meeky92 <112586695+meeky92@users.noreply.github.com>
2022-10-05 09:28:26 -0500

    first commit

---
+++ test_module.py
@@ -0,0 +1,10 @@
+print('Hello this is the test module!')
+
+def greet(name):
+    print(f"Hello {name.title()}. How are you today? I hope you are well!")
+
+def leave(name):
+    print(f"Goodbye {name.title()}. It was a pleasure seeing you.")
+
+greet('eunice')
+leave('eunice')
